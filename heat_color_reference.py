e5ee541fee1c1de91c04647d14f0ed424232b5f4
ss-krishna-sumanth <sumanthgarlap@gmail.com>
2020-03-28 17:43:50 +0530

    new color scheme

--- heat_color_reference.py
+++ heat_color_reference.py
@@ -10,9 +10,12 @@ def helper(st):
 	return st 
 
 def rgb_vals(data_point):
-	r = min(max(0, 1.5-abs(1-4*(data_point-0.5))),1)
-	g = min(max(0, 1.5-abs(1-4*(data_point-0.25))),1)
-	b = min(max(0, 1.5-abs(1-4*data_point)),1)
+	#r=\min(\max(0,1.2-\operatorname{abs}(1.1-2*(x-0.38))),1)
+	r = min(max(0, 1.2-abs(1.1-2*(data_point-0.38))),1)
+	#g=\min(\max(0,1.5-\operatorname{abs}(1-3*(x-0.16))),1)
+	g = min(max(0, 1.5-abs(1-3*(data_point-0.16))),1)
+	#b=\min(\max(0,1.5-\operatorname{abs}(0.-4*x)),1)
+	b = min(max(0, 1.5-abs(0-4*data_point)),1)
 
 	R = hex(int(r*255)).lstrip("0x")
 	G = hex(int(g*255)).lstrip("0x")
