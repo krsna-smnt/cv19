2516b68c2b89ac2121ad0cafe00a7e3d3d1117b1
Abhay <abhaytyagi1998@gmail.com>
2020-04-14 04:12:31 +0000

    gecko path

--- cv19/website/management/commands/worldData.py
+++ cv19/website/management/commands/worldData.py
@@ -38,9 +38,9 @@ def init_driver():
     options = Options()
     options.headless = True
 
-    driver = webdriver.Firefox(options=options)
+    driver = webdriver.Firefox(options=options, executable_path='/usr/local/bin/geckodriver')
     driver.get(url)
-
+    print("init")
     return True
 
 
@@ -65,7 +65,7 @@ def unpack_info(info):
             raw[i] = float(raw[i])
         else:
             raw[i] = int(raw[i])
-
+    print("unpkd")
     #Format:
     #[country_name (type: string), total_cases (type: int), *new_cases (type: int), total_dead (type: int), new_dead (type: int), total_recovered (type: int), active_cases (type: int), critical_cases (type: int), cases_per_million (type: int), deaths_per_million (type: int), first_case_date (type: string)]
     return raw
@@ -101,6 +101,7 @@ class Command(BaseCommand):
 
         for item in items_set:
             ret = unpack_info(item)
+            print(ret[0])
             if ret[0] in 'World':
                 continue
             max_infected_ppm = max(max_infected_ppm, ret[1])
@@ -111,40 +112,42 @@ class Command(BaseCommand):
 
         for item in items_set:
             ret = unpack_info(item)
+            print(ret[0])
+ #           try:
+            f.writerow(ret)
+            country = Country.objects.get(name=ret[0])
+
+            oldn = country.total_cases
+            newn = ret[1]
+
+            country.total_cases = ret[1]
+            country.new_infected = ret[2]
+            country.infected = ret[6]
+            country.new_dead = ret[4]
+            country.dead = ret[3]
+            country.cured = ret[5]
+            country.critical = ret[7]
+            country.cases_per_million = ret[8]
+            country.dead_per_million = ret[9]
 
             try:
-                f.writerow(ret)
-                country = Country.objects.get(name=ret[0])
+                if oldn != newn:
+                    country.percentage_increase = round(100 * int(country.new_infected) / int(country.total_cases), 2)
+            except ZeroDivisionError:
+                country.percentage_increase = 0
 
-                oldn = country.total_cases
-                newn = ret[1]
-
-                country.total_cases = ret[1]
-                country.new_infected = ret[2]
-                country.infected = ret[6]
-                country.new_dead = ret[4]
-                country.dead = ret[3]
-                country.cured = ret[5]
-                country.critical = ret[7]
-                country.cases_per_million = ret[8]
-                country.dead_per_million = ret[9]
-
-                try:
-                    if oldn != newn:
-                        country.percentage_increase = round(100 * int(country.new_infected) / int(country.total_cases), 2)
-                except ZeroDivisionError:
-                    country.percentage_increase = 0
-
-                if 'World' in ret[0]:
-                    country.save()
-                    continue
-
-                infected_ppm_color = rgb_vals(normalize0_1(ret[1], max_infected_ppm, min_infected_ppm))
-                dead_ppm_color = rgb_vals(normalize0_1(ret[3], max_dead_ppm, min_dead_ppm))
-
-                country.infected_color = infected_ppm_color
-                country.dead_color = dead_ppm_color
+            if 'World' in ret[0]:
                 country.save()
+                continue
 
-            except:
-                print("no such country")
+            infected_ppm_color = rgb_vals(normalize0_1(ret[1], max_infected_ppm, min_infected_ppm))
+            dead_ppm_color = rgb_vals(normalize0_1(ret[3], max_dead_ppm, min_dead_ppm))
+
+            country.infected_color = infected_ppm_color
+            country.dead_color = dead_ppm_color
+            country.save()
+
+#            except:
+#                print("no such country")
+
+        driver.quit()
